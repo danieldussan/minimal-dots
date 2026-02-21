2e5f7bdac62a538ec429ba461e26831bfa392560
Daniel Dussan <daniel2014dussan@gmail.com>
2026-02-21 01:04:47 -0500

    feat(setup): add option to configure environment for ROOT user

--- setup.sh
+++ setup.sh
@@ -41,4 +41,35 @@ echo -e "${BLUE}Cambiando shell a ZSH...${NC}"
 ZSH_PATH=$(which zsh)
 sudo chsh -s "$ZSH_PATH" "$USER"
 
+# 7. Preguntar si se quiere configurar para ROOT
+echo -en "${BLUE}¿Deseas configurar este entorno también para el usuario ROOT? (s/n): ${NC}"
+read -r response
+
+if [[ "$response" =~ ^([sS][iI]|[sS]|[yY][eE][sS]|[yY])$ ]]; then
+  echo -e "${BLUE}Configurando entorno para ROOT...${NC}"
+  # Limpieza para root
+  sudo rm -rf "/root/.oh-my-zsh"
+  sudo rm -f "/root/.zshrc"
+  sudo rm -f "/root/.p10k.zsh"
+
+  # Instalación de Oh My Zsh para root
+  sudo HOME=/root sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+  # Plugins y Tema para root
+  ROOT_ZSH_CUSTOM="/root/.oh-my-zsh/custom"
+  sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ROOT_ZSH_CUSTOM}"/themes/powerlevel10k
+  sudo git clone https://github.com/zsh-users/zsh-autosuggestions "${ROOT_ZSH_CUSTOM}"/plugins/zsh-autosuggestions
+  sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ROOT_ZSH_CUSTOM}"/plugins/zsh-syntax-highlighting
+
+  # Copiar configuración (puedes bajarlos de nuevo o copiarlos del usuario actual)
+  sudo curl -fsSL "${REPO_URL}/.zshrc" -o /root/.zshrc
+  sudo curl -fsSL "${REPO_URL}/.p10k.zsh" -o /root/.p10k.zsh
+
+  # Cambiar shell de root
+  sudo chsh -s "$ZSH_PATH" root
+  echo -e "${GREEN}Entorno de ROOT configurado.${NC}"
+else
+  echo -e "${BLUE}Saltando configuración de ROOT.${NC}"
+fi
+
 echo -e "${GREEN}¡Todo listo! Sal de la sesión o escribe 'zsh' para empezar.${NC}"
